2a2812f3308b15c1b6e680248289bc229d04ba47
Hughian <769156619@qq.com>
2020-03-24 15:33:27 +0800

    fix

---
+++ models/__init__.py
@@ -0,0 +1,15 @@
+
+from .AFM import AFM
+from .AutoInt import AutoInt
+from .CCPM import CCPM
+from .DCN import DCN
+from .DeepFM import DeepFM
+from .FGCNN import FGCNN
+from .FiBiNet import FiBiNet
+from .FNN import FNN
+from .MLR import MLR
+from .NFFM import NFFM
+from .NFM import NFM
+from .PNN import PNN
+from .WideAndDeep import WideAndDeep
+from .xDeepFM import xDeepFM
